316dbf4db26d9f7073a93a2b74290089da6447f1
Andrei <Andreiexample@yandex.com>
2018-02-14 18:27:05 +0300

    Task 3 corrected

--- Task3/src/HeadsOrTails/HeadsOrTails.java
+++ Task3/src/HeadsOrTails/HeadsOrTails.java
@@ -4,9 +4,10 @@ import java.util.Random;
 
 public class HeadsOrTails {
     public int countHeads(int attempt){
+        Random random = new Random();
         int headsWas = 0;
         for (int i = 0; i <= attempt; i++) {
-            if (new Random().nextBoolean()) {
+            if (random.nextBoolean()) {
                 headsWas++;
             }
         }
