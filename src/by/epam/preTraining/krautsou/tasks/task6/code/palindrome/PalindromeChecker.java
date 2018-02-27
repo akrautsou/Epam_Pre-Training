8310de9ad072179678c1c1ef0a4a1e58ce9f50ae
Andrei <Andreiexample@yandex.com>
2018-02-27 22:45:19 +0300

    task 6 (finish) + add Big - O

--- src/by/epam/preTraining/krautsou/tasks/task6/code/palindrome/PalindromeChecker.java
+++ src/by/epam/preTraining/krautsou/tasks/task6/code/palindrome/PalindromeChecker.java
@@ -4,14 +4,16 @@ import by.epam.preTraining.krautsou.tasks.task6.code.IStack;
 
 public class PalindromeChecker {
     public static boolean isPalindrome(String string, IStack customStack) {
-        string = string.toUpperCase();
-        StringBuilder reversedWord = new StringBuilder();
+        string = string.toUpperCase();//O(1)
+        StringBuilder reversedWord = new StringBuilder(); //O(1)
         for (int i = 0; i < string.length(); i++) {
-            customStack.push(string.charAt(i));
+            customStack.push(string.charAt(i));  //O(n) )
         }
         for (int i = customStack.getSize() - 1; i >= 0; i--) {
-            reversedWord.append(customStack.pop());
+            reversedWord.append(customStack.pop());//O(n) )
         }
-        return string.equals(reversedWord.toString());
+        return string.equals(reversedWord.toString());//O(1)
+        //O(1) + O(1) + O(n) + O(n) + O(1)  = O(n)
+        //где n - размерность строки
     }
 }
